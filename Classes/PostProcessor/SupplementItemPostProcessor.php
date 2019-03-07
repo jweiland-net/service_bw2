682ae80ef95b073d7bf537780e352deeecd718d8
Pascal Rinker <prinker@jweiland.net>
2019-03-07 10:49:28 +0100

    [RELEASE] 1.6.0
    - Feature: Add more checks to return just public and valid items

--- Classes/PostProcessor/SupplementItemPostProcessor.php
+++ Classes/PostProcessor/SupplementItemPostProcessor.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 namespace JWeiland\ServiceBw2\PostProcessor;
 
 /*
@@ -27,13 +27,23 @@ class SupplementItemPostProcessor extends AbstractPostProcessor
     public function process($response): array
     {
         $response = (array)$response;
-        foreach ($response as $key => $value) {
+        foreach ($response as $key => $item) {
+            // Check if leistung is a leistung or a zustaendigkeit with a leistung as array item
+            if (array_key_exists('leistung', $item) && is_array($item['leistung'])) {
+                $leistung = $item['leistung'];
+                $originalId = $leistung['landesZustaendigkeitId'];
+            } else {
+                $leistung = $item;
+                $originalId = $leistung['landesLeistungId'];
+            }
+
+            // Unset original items if current item is a replacement of it
             if (
-                array_key_exists('type', $value)
-                && $value['type'] === 'ERGAENZUNG'
-                && array_key_exists($value['landesLeistungId'], $response)
+                array_key_exists('type', $leistung)
+                && array_key_exists($originalId, $response)
+                && $leistung['type'] === 'ERGAENZUNG'
             ) {
-                unset($response[$value['landesLeistungId']]);
+                unset($response[$originalId]);
             }
         }
         return $response;
--- Classes/Request/Zustaendigkeiten/Leistung.php
+++ Classes/Request/Zustaendigkeiten/Leistung.php
@@ -1,8 +1,8 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 namespace JWeiland\ServiceBw2\Request\Zustaendigkeiten;
 
 /*
-* This file is part of the TYPO3 CMS project.
+* This file is part of the service_bw2 project.
 *
 * It is free software; you can redistribute it and/or modify it under
 * the terms of the GNU General Public License, either version 2
@@ -14,6 +14,7 @@ namespace JWeiland\ServiceBw2\Request\Zustaendigkeiten;
 * The TYPO3 project - inspiring people to share!
 */
 
+use JWeiland\ServiceBw2\PostProcessor\PublishStatusPostProcessor;
 use JWeiland\ServiceBw2\Request\AbstractRequest;
 
 /**
@@ -28,6 +29,13 @@ class Leistung extends AbstractRequest
      * @var string
      */
     protected $path = '/zustaendigkeiten/leistung/{leistungId}/?page={page}&pageSize={pageSize}';
+
+    /**
+     * @var array
+     */
+    protected $additionalPostProcessorClassNames = [
+        PublishStatusPostProcessor::class
+    ];
 
     /**
      * @var array
