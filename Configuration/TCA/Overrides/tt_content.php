383030370c6c76361904aac663006dfe9a03acc3
Stefan Froemken <froemken@gmail.com>
2019-07-29 16:53:42 +0200

    Move TCA changing lines of ext_tables.php to Overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,4 +1,18 @@
 <?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['servicebw2_servicebw'] = 'pi_flexform';
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['servicebw2_servicebw'] = 'select_key';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('servicebw2_servicebw', 'FILE:EXT:service_bw2/Configuration/FlexForms/ServiceBw2.xml');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'servicebw2_servicebw',
+    'FILE:EXT:service_bw2/Configuration/FlexForms/ServiceBw2.xml'
+);
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'JWeiland.service_bw2',
+    'ServiceBw',
+    'LLL:EXT:service_bw2/Resources/Private/Language/locallang_db.xlf:plugin.title'
+);
