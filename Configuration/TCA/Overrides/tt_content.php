0f7748d865ff4e59213f44d04eab2eb755e0e3bd
Stefan Froemken <froemken@gmail.com>
2020-01-29 14:04:47 +0100

    Add new entry for service_bw2 in newContentElementWizard

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,5 +13,5 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['serviceb
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'JWeiland.service_bw2',
     'ServiceBw',
-    'LLL:EXT:service_bw2/Resources/Private/Language/locallang_db.xlf:plugin.title'
+    'LLL:EXT:service_bw2/Resources/Private/Language/locallang_db.xlf:plugin.servicebw.title'
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,37 +1,61 @@
 <?php
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'JWeiland.service_bw2',
-    'ServiceBw',
-    [
-        'Organisationseinheiten' => 'list,show',
-        'Lebenslagen' => 'list,show',
-        'Leistungen' => 'list,show'
-    ],
-    // non-cacheable actions
-    []
-);
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
 
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['servicebw_request'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['servicebw_request'] = [
-        'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
-        'backend' => \TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class,
-        'options' => [],
-        'groups' => ['all', 'pages']
+call_user_func(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'JWeiland.service_bw2',
+        'ServiceBw',
+        [
+            'Organisationseinheiten' => 'list,show',
+            'Lebenslagen' => 'list,show',
+            'Leistungen' => 'list,show'
+        ],
+        // non-cacheable actions
+        []
+    );
+
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['servicebw_request'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['servicebw_request'] = [
+            'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+            'backend' => \TYPO3\CMS\Core\Cache\Backend\Typo3DatabaseBackend::class,
+            'options' => [],
+            'groups' => ['all', 'pages']
+        ];
+    }
+
+    if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
+        // RealUrl auto configuration
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['service_bw2'] = \JWeiland\ServiceBw2\Hooks\RealUrlAutoConfiguration::class . '->addConfig';
+    }
+
+    // Remove sys_registry entry, if System Cache was cleared, to allow switching the Authentication
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['events2_clearcache'] = \JWeiland\ServiceBw2\Hooks\DataHandler::class . '->clearCachePostProc';
+
+    // Register SVG Icon Identifier
+    $svgIcons = [
+        'ext-servicebw-wizard-icon' => 'plugin_wizard.svg',
     ];
-}
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    foreach ($svgIcons as $identifier => $fileName) {
+        $iconRegistry->registerIcon(
+            $identifier,
+            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+            ['source' => 'EXT:service_bw2/Resources/Public/Icons/' . $fileName]
+        );
+    }
 
-if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('realurl')) {
-    // RealUrl auto configuration
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['service_bw2'] = \JWeiland\ServiceBw2\Hooks\RealUrlAutoConfiguration::class . '->addConfig';
-}
+    // add service_bw2 to new element wizard
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:service_bw2/Configuration/TSconfig/ContentElementWizard.txt">'
+    );
 
-// Remove sys_registry entry, if System Cache was cleared, to allow switching the Authentication
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['events2_clearcache'] = \JWeiland\ServiceBw2\Hooks\DataHandler::class . '->clearCachePostProc';
-
-// create scheduler to create/update solr index for service_bw2
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\JWeiland\ServiceBw2\Task\IndexItemsTask::class] = [
-    'extension' => 'service_bw2',
-    'title' => 'Index service_bw2',
-    'description' => 'Re-Generate solr index for service_bw2',
-    'additionalFields' => \JWeiland\ServiceBw2\Task\IndexItemsTaskAdditionalFieldProvider::class
-];
+    // Create scheduler to create/update solr index for service_bw2
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\JWeiland\ServiceBw2\Task\IndexItemsTask::class] = [
+        'extension' => 'service_bw2',
+        'title' => 'Index service_bw2',
+        'description' => 'Re-Generate solr index for service_bw2',
+        'additionalFields' => \JWeiland\ServiceBw2\Task\IndexItemsTaskAdditionalFieldProvider::class
+    ];
+});
