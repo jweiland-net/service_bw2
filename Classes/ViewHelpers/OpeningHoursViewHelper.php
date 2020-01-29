d7600737759af6f7afdb429675e6b2553505c07f
Stefan Froemken <froemken@gmail.com>
2020-01-29 14:29:02 +0100

    Use AbstractViewHelper of Typo3fluid package

--- Classes/ViewHelpers/OpeningHoursViewHelper.php
+++ Classes/ViewHelpers/OpeningHoursViewHelper.php
@@ -16,8 +16,8 @@ namespace JWeiland\ServiceBw2\ViewHelpers;
  */
 
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 /**
