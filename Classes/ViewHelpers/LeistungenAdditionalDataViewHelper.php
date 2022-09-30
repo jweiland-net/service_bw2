6b0ca4278ab558aa78659f3e9c14716b79399285
Stefan Froemken <froemken@gmail.com>
2022-09-30 15:29:30 +0200

    Update code structure in VH for better readability

--- Classes/ViewHelpers/LeistungenAdditionalDataViewHelper.php
+++ Classes/ViewHelpers/LeistungenAdditionalDataViewHelper.php
@@ -32,8 +32,20 @@ class LeistungenAdditionalDataViewHelper extends AbstractViewHelper
     public function initializeArguments(): void
     {
         parent::initializeArguments();
-        $this->registerArgument('id', 'int', 'ID of the Leistung record', true);
-        $this->registerArgument('as', 'string', 'Name of the variable that contains the additional data', true);
+
+        $this->registerArgument(
+            'id',
+            'int',
+            'ID of the Leistung record',
+            true
+        );
+
+        $this->registerArgument(
+            'as',
+            'string',
+            'Name of the variable that contains the additional data',
+            true
+        );
     }
 
     public static function renderStatic(
@@ -41,12 +53,19 @@ class LeistungenAdditionalDataViewHelper extends AbstractViewHelper
         \Closure $renderChildrenClosure,
         RenderingContextInterface $renderingContext
     ) {
-        $leistungenHelper = GeneralUtility::makeInstance(LeistungenHelper::class);
         $templateVariableContainer = $renderingContext->getVariableProvider();
-        $templateVariableContainer->add($arguments['as'], $leistungenHelper->getAdditionalData((int)$arguments['id']));
+        $templateVariableContainer->add(
+            $arguments['as'],
+            self::getLeistungenHelper()->getAdditionalData((int)$arguments['id'])
+        );
         $output = $renderChildrenClosure();
         $templateVariableContainer->remove($arguments['as']);
 
         return $output;
+    }
+
+    protected static function getLeistungenHelper(): LeistungenHelper
+    {
+        return GeneralUtility::makeInstance(LeistungenHelper::class);
     }
 }
