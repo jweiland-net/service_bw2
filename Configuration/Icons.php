654500e3bf3ad59b13f186d1622fde84777c7a8a
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-01-11 19:46:18 +0100

    Icons updated for new plugins

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -6,4 +6,32 @@ return [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         'source' => 'EXT:service_bw2/Resources/Public/Icons/plugin_wizard.svg'
     ],
+    'ext-servicebw2-organizationalunitslist-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:service_bw2/Resources/Public/Icons/servicebw2_organizationalunitslist.svg'
+    ],
+    'ext-servicebw2-organizationalunitsshow-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:service_bw2/Resources/Public/Icons/servicebw2_organizationalunitsshow.svg'
+    ],
+    'ext-servicebw2-serviceslist-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:service_bw2/Resources/Public/Icons/servicebw2_serviceslist.svg'
+    ],
+    'ext-servicebw2-servicesshow-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:service_bw2/Resources/Public/Icons/servicebw2_servicesshow.svg'
+    ],
+    'ext-servicebw2-lifesituationslist-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:service_bw2/Resources/Public/Icons/servicebw2_lifesituationslist.svg'
+    ],
+    'ext-servicebw2-lifesituationsshow-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:service_bw2/Resources/Public/Icons/servicebw2_lifesituationsshow.svg'
+    ],
+    'ext-servicebw2-servicebw2search-icon' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:service_bw2/Resources/Public/Icons/servicebw2_servicebw2search.svg'
+    ],
 ];
