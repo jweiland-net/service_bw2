e7df809d116cc7734a3d8254988eacf0d9676c85
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-04-09 13:41:05 +0200

    [TASK] Fixed version compatibilites in configuration files for installation

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,18 +8,18 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '7.0.2',
+    'version' => '8.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.9-12.4.99',
-            'scheduler' => '12.4.0-12.4.99',
-            'maps2' => '11.0.0-0.0.0',
+            'typo3' => '13.4.3-13.4.99',
+            'scheduler' => '13.4.0-0.0.0',
+            'maps2' => '12.0.0-0.0.0',
         ],
         'conflicts' => [
             'fal_dropbox' => '',
         ],
         'suggests' => [
-            'solr' => '12.0.0-0.0.0',
+            'solr' => '13.0.0-0.0.0',
         ],
     ],
 ];
