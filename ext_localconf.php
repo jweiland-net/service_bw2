7267d91051e9d5a3bf34fcd2b297b4a3448585b2
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-05-05 09:27:13 +0200

    [TASK] Formatting issues fixed

--- ext_localconf.php
+++ ext_localconf.php
@@ -23,113 +23,111 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-call_user_func(static function () {
-    // 1. Organizational Units List Plugin
-    ExtensionUtility::configurePlugin(
-        'ServiceBw2',
-        'OrganizationalUnitsList',
-        [
-            OrganisationseinheitenController::class => 'list, show',
-        ],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-    // 2. Organizational Units Show Plugin
-    ExtensionUtility::configurePlugin(
-        'ServiceBw2',
-        'OrganizationalUnitsShow',
-        [
-            OrganisationseinheitenController::class => 'show',
-        ],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-    // 3. Services List Plugin
-    ExtensionUtility::configurePlugin(
-        'ServiceBw2',
-        'ServicesList',
-        [
-            LeistungenController::class => 'list, show',
-        ],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-    // 4. Services Show Plugin
-    ExtensionUtility::configurePlugin(
-        'ServiceBw2',
-        'ServicesShow',
-        [
-            LeistungenController::class => 'show',
-        ],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-    // 5. Life Situations List Plugin
-    ExtensionUtility::configurePlugin(
-        'ServiceBw2',
-        'LifeSituationsList',
-        [
-            LebenslagenController::class => 'list, show',
-        ],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-    // 6. Life Situations Show Plugin
-    ExtensionUtility::configurePlugin(
-        'ServiceBw2',
-        'LifeSituationsShow',
-        [
-            LebenslagenController::class => 'show',
-        ],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
-    // 7. Search Plugin
-    ExtensionUtility::configurePlugin(
-        'ServiceBw2',
-        'ServiceBw2Search',
-        [
-            SucheController::class => 'list',
-        ],
-        [],
-        ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
-    );
+// 1. Organizational Units List Plugin
+ExtensionUtility::configurePlugin(
+    'ServiceBw2',
+    'OrganizationalUnitsList',
+    [
+        OrganisationseinheitenController::class => 'list, show',
+    ],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
+// 2. Organizational Units Show Plugin
+ExtensionUtility::configurePlugin(
+    'ServiceBw2',
+    'OrganizationalUnitsShow',
+    [
+        OrganisationseinheitenController::class => 'show',
+    ],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
+// 3. Services List Plugin
+ExtensionUtility::configurePlugin(
+    'ServiceBw2',
+    'ServicesList',
+    [
+        LeistungenController::class => 'list, show',
+    ],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
+// 4. Services Show Plugin
+ExtensionUtility::configurePlugin(
+    'ServiceBw2',
+    'ServicesShow',
+    [
+        LeistungenController::class => 'show',
+    ],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
+// 5. Life Situations List Plugin
+ExtensionUtility::configurePlugin(
+    'ServiceBw2',
+    'LifeSituationsList',
+    [
+        LebenslagenController::class => 'list, show',
+    ],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
+// 6. Life Situations Show Plugin
+ExtensionUtility::configurePlugin(
+    'ServiceBw2',
+    'LifeSituationsShow',
+    [
+        LebenslagenController::class => 'show',
+    ],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
+// 7. Search Plugin
+ExtensionUtility::configurePlugin(
+    'ServiceBw2',
+    'ServiceBw2Search',
+    [
+        SucheController::class => 'list',
+    ],
+    [],
+    ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT,
+);
 
-    // Create our own logger file
-    if (!isset($GLOBALS['TYPO3_CONF_VARS']['LOG']['JWeiland']['ServiceBw2']['writerConfiguration'])) {
-        $GLOBALS['TYPO3_CONF_VARS']['LOG']['JWeiland']['ServiceBw2']['writerConfiguration'] = [
-            LogLevel::INFO => [
-                FileWriter::class => [
-                    'logFileInfix' => 'servicebw2',
-                ],
+// Create our own logger file
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['LOG']['JWeiland']['ServiceBw2']['writerConfiguration'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['LOG']['JWeiland']['ServiceBw2']['writerConfiguration'] = [
+        LogLevel::INFO => [
+            FileWriter::class => [
+                'logFileInfix' => 'servicebw2',
             ],
-        ];
-    }
-
-    // Register caches for requests to Service BW API.
-    // Set group to system. So, pages cache can be flushed, without lost of Service BW data.
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['servicebw_request'] = [
-        'frontend' => VariableFrontend::class,
-        'backend' => Typo3DatabaseBackend::class,
-        'options' => [],
-        'groups' => [
-            0 => 'system',
         ],
     ];
+}
 
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['servicebw_additionalstuff'] = [
-        'frontend' => VariableFrontend::class,
-        'backend' => Typo3DatabaseBackend::class,
-        'options' => [],
-        'groups' => [
-            0 => 'system',
-        ],
-    ];
+// Register caches for requests to Service BW API.
+// Set group to system. So, pages cache can be flushed, without lost of Service BW data.
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['servicebw_request'] = [
+    'frontend' => VariableFrontend::class,
+    'backend' => Typo3DatabaseBackend::class,
+    'options' => [],
+    'groups' => [
+        0 => 'system',
+    ],
+];
 
-    // Remove sys_registry entry, if System Cache was cleared, to allow switching the Authentication
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['servicebw2_clearcache']
-        = ClearCacheHook::class . '->clearCachePostProc';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['servicebw_additionalstuff'] = [
+    'frontend' => VariableFrontend::class,
+    'backend' => Typo3DatabaseBackend::class,
+    'options' => [],
+    'groups' => [
+        0 => 'system',
+    ],
+];
 
-    // Register an Aspect to map various ID of Service BW API to uid-title
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['ServiceBwTitleMapper'] = ServiceBwTitleMapper::class;
-});
+// Remove sys_registry entry, if System Cache was cleared, to allow switching the Authentication
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc']['servicebw2_clearcache']
+    = ClearCacheHook::class . '->clearCachePostProc';
+
+// Register an Aspect to map various ID of Service BW API to uid-title
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['ServiceBwTitleMapper'] = ServiceBwTitleMapper::class;
--- ext_tables.sql
+++ ext_tables.sql
@@ -3,8 +3,8 @@
 #
 CREATE TABLE tx_servicebw2_organisationseinheit
 (
-	id             INT(11) DEFAULT 0            NOT NULL,
-	hashed_address CHAR(32) DEFAULT '' NOT NULL,
-	tx_maps2_poi   INT(11) UNSIGNED DEFAULT '0' NOT NULL,
+	id             INT(11)							DEFAULT	0		NOT	NULL,
+	hashed_address CHAR(32)							DEFAULT ''	NOT NULL,
+	tx_maps2_poi   INT(11)		UNSIGNED	DEFAULT	0		NOT	NULL,
 	PRIMARY KEY (id)
 );
