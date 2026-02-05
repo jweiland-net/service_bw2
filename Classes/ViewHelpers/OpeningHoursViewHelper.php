fae80211adaaf1eac7bfca1079caa2df81b26271
Stefan Froemken <sfroemken@jweiland.net>
2026-02-05 15:31:53 +0100

    Remove CompileableRenderStatic trait from VHs

--- Classes/ViewHelpers/OpeningHoursViewHelper.php
+++ Classes/ViewHelpers/OpeningHoursViewHelper.php
@@ -12,33 +12,27 @@ declare(strict_types=1);
 namespace JWeiland\ServiceBw2\ViewHelpers;
 
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
-use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 /**
  * ViewHelper to render opening hours provided by Service BW API
  * Service BW brings a very weird structure so you should check
  * organisationseinheit.openingHours (HTML opening hours) if you
- * don´t get structuredOpeningHours (this weird array) from the
+ * don't get structuredOpeningHours (this weird array) from the
  * API
- * Will return the following structure (multi language):
+ * Will return the following structure (multi-language):
  * <dl class="extdl clearfix">
  *   <dt>Opening hours</dt>
  *   <dd>Mon 08-12</dd>
  *   <dd>Tues 08-12</dd>
  *     ...
  * </dl>
- * Displays all opening hours using 24 hours system
+ * Displays all opening hours using 24-hour system
  */
-class OpeningHoursViewHelper extends AbstractViewHelper
+final class OpeningHoursViewHelper extends AbstractViewHelper
 {
-    use CompileWithRenderStatic;
-
     /**
      * As this ViewHelper renders HTML, the output must not be escaped.
-     *
-     * @var bool
      */
     protected $escapeOutput = false;
 
@@ -65,11 +59,8 @@ class OpeningHoursViewHelper extends AbstractViewHelper
     /**
      * Returns the opening hours for a given opening hours array
      */
-    public static function renderStatic(
-        array $arguments,
-        \Closure $renderChildrenClosure,
-        RenderingContextInterface $renderingContext,
-    ): string {
+    public function render(): string
+    {
         $html = [];
         $allowedTypes = [
             'ALLGEMEINE_OEFFNUNGSZEIT',
@@ -80,13 +71,14 @@ class OpeningHoursViewHelper extends AbstractViewHelper
             'SERVICEZEIT',
             'TELEFONISCHE_ERREICHBARKEIT',
         ];
+
         // This array inlcudes an array from type ALLGEMEINE_OEFFNUNGSZEIT
-        foreach ($arguments['oeffnungszeitenStrukturiert'] as $structuredOpeningHours) {
+        foreach ($this->arguments['oeffnungszeitenStrukturiert'] as $structuredOpeningHours) {
             if (
                 isset($structuredOpeningHours['typ'])
                 && in_array($structuredOpeningHours['typ'], $allowedTypes, true)
             ) {
-                $html[] = self::getStructuredOpeningHoursHTML($structuredOpeningHours);
+                $html[] = $this->getStructuredOpeningHoursHTML($structuredOpeningHours);
             }
         }
 
@@ -94,9 +86,9 @@ class OpeningHoursViewHelper extends AbstractViewHelper
     }
 
     /**
-     * Get HTML for an structuredOpeningHours array
+     * Get HTML for a structuredOpeningHours array
      */
-    protected static function getStructuredOpeningHoursHTML(array $structuredOpeningHours): string
+    private function getStructuredOpeningHoursHTML(array $structuredOpeningHours): string
     {
         $html = [];
         if (isset($structuredOpeningHours['regulaereZeiten']) && is_array($structuredOpeningHours['regulaereZeiten'])) {
@@ -105,7 +97,7 @@ class OpeningHoursViewHelper extends AbstractViewHelper
             // Afternoon opening hours
             $afternoonOpeningHours = [];
 
-            self::processOpeningHours(
+            $this->processOpeningHours(
                 $structuredOpeningHours['regulaereZeiten'],
                 $forenoonOpeningHoursWorkdays,
                 $afternoonOpeningHours,
@@ -160,7 +152,7 @@ class OpeningHoursViewHelper extends AbstractViewHelper
     /**
      * Process opening hours using $structuredOpeningHours['regulaereZeiten'] array
      */
-    protected static function processOpeningHours(
+    private function processOpeningHours(
         array $regulaereZeiten,
         array &$forenoonOpeningHoursWorkdays,
         array &$afternoonOpeningHours,
@@ -174,11 +166,11 @@ class OpeningHoursViewHelper extends AbstractViewHelper
             ) {
                 // Opening hours monday to friday forenoon
                 if ($regulaereZeitenDay['tagestyp'] === 'ARBEITSTAG_MO_FR') {
-                    $regulaereZeitenHours = self::getRegulaereZeitenHours($regulaereZeitenDay);
+                    $regulaereZeitenHours = $this->getRegulaereZeitenHours($regulaereZeitenDay);
                     $forenoonOpeningHoursWorkdays[(int)substr($regulaereZeitenHours, 0, 2)] = $regulaereZeitenHours;
                 } else {
                     // Opening hours individual day afternoon
-                    $regulaereZeitenHours = self::getRegulaereZeitenHours($regulaereZeitenDay);
+                    $regulaereZeitenHours = $this->getRegulaereZeitenHours($regulaereZeitenDay);
                     $afternoonOpeningHours[$regulaereZeitenDay['tagestyp']][(int)substr($regulaereZeitenHours, 0, 2)]
                         = $regulaereZeitenHours;
                 }
@@ -187,9 +179,9 @@ class OpeningHoursViewHelper extends AbstractViewHelper
     }
 
     /**
-     * Get the opening hours as string e.g. 07:00 - 12:00
+     * Get the opening hours as a string e.g., 07:00 - 12:00
      */
-    protected static function getRegulaereZeitenHours(array $regulaereZeiten): string
+    private function getRegulaereZeitenHours(array $regulaereZeiten): string
     {
         return date('H:i', $regulaereZeiten['beginn'] / 1000) . ' - ' . date('H:i', $regulaereZeiten['ende'] / 1000);
     }
